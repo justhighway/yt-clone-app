b40ef69fcb08a31f1ce42751dc1baf9408a6bfd1
justhighway <hirouth@naver.com>
2024-12-03 20:39:30 +0900

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { ThemeProvider } from "@/providers/ThemeProvider";
+import { ThemeProvider } from "@/providers/themeProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
